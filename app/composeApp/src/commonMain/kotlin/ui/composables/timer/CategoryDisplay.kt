5fb12aae248d51de5d1538458f025e0bf4b85a9a
kuromiichi <emmalikescoding@gmail.com>
2024-05-23 21:00:21 +0200

    CategoryDisplay

---
+++ app/composeApp/src/commonMain/kotlin/ui/composables/timer/CategoryDisplay.kt
@@ -0,0 +1,55 @@
+package ui.components.timer
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryDisplay(
+    categoryName: String,
+    subcategoryName: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.tertiary,
+            shadowElevation = 4.dp,
+            modifier = Modifier.weight(2f)
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(
+                    categoryName,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.tertiary,
+            shadowElevation = 4.dp,
+            modifier = Modifier.weight(3f)
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(
+                    subcategoryName,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
