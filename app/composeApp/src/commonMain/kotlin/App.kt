9edaf06b5821b0b150248b5cae74bfbf515fac10
sergiomisas <misas_virseda@hotmail.es>
2024-06-03 17:57:42 +0200

    Basic Bottom Tab Bar Implementation (Voyager)

--- app/composeApp/src/commonMain/kotlin/App.kt
+++ app/composeApp/src/commonMain/kotlin/App.kt
@@ -1,5 +1,19 @@
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import ui.navigation.SettingsTab
+import ui.navigation.SolveListTab
+import ui.navigation.TimerTab
+import ui.screens.timer.TimerScreen
 import ui.theme.OmniTimerTheme
 
 
@@ -7,6 +21,35 @@ import ui.theme.OmniTimerTheme
 @Preview
 fun App() {
     OmniTimerTheme {
-        /* APP CONTENT */
+        TabNavigator(TimerTab){
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        TabItem(SolveListTab)
+                        TabItem(TimerTab)
+                        TabItem(SettingsTab)
+                    }
+                }
+            ){
+                CurrentTab()
+            }
+        }
+
     }
+}
+
+@Composable
+private fun RowScope.TabItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = {
+            tabNavigator.current = tab
+        },
+        icon = {
+            tab.options.icon?.let { painter ->
+                Icon(painter, contentDescription = tab.options.title)
+            }
+        }
+    )
 }
---
+++ app/composeApp/src/commonMain/kotlin/ui/navigation/SolveListTab.kt
@@ -0,0 +1,36 @@
+package ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+import ui.screens.settings.SettingsScreen
+
+object SolveListTab: Tab {
+    override val options: TabOptions
+        @Composable
+        get() {
+            val icon = rememberVectorPainter(Icons.Default.Info)
+            val title = "Solve List"
+            val index: UShort = 0u
+
+            return TabOptions(
+                icon = icon,
+                title = title,
+                index = index
+            )
+        }
+
+    @Composable
+    override fun Content() {
+        Surface {
+            Text("Solve List")
+        }
+    }
+}
