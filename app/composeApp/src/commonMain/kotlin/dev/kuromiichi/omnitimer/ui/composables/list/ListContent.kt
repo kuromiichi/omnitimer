69ca7700fb843d52955ff983b364b75d41c5bf97
kuromiichi <emmalikescoding@gmail.com>
2024-06-11 22:42:04 +0200

    Fixed scramble images rendering too small in Android

--- app/composeApp/src/commonMain/kotlin/dev/kuromiichi/omnitimer/ui/composables/list/ListContent.kt
+++ app/composeApp/src/commonMain/kotlin/dev/kuromiichi/omnitimer/ui/composables/list/ListContent.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Delete
@@ -20,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,138 +64,6 @@ fun ListContent(
 }
 
 @Composable
-private fun ContentLandscape(
-    modifier: Modifier,
-    image: String,
-    scramble: String,
-    selectedPenalty: Status,
-    onPenaltySelected: (Status) -> Unit,
-    onDeleteClick: () -> Unit,
-    onArchiveClick: () -> Unit
-) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Row {
-            Image(
-                modifier = Modifier.weight(1f),
-                bitmap = image.toImageBitmap(),
-                contentDescription = null
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = scramble,
-                style = MaterialTheme.typography.headlineSmall,
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Surface(
-            modifier = Modifier.fillMaxWidth(),
-            color = MaterialTheme.colorScheme.secondary,
-            shadowElevation = 4.dp,
-            shape = MaterialTheme.shapes.small
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-            ) {
-                Button(
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = if (selectedPenalty == Status.OK) {
-                            MaterialTheme.colorScheme.secondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.secondary
-                        },
-                        contentColor = if (selectedPenalty == Status.OK) {
-                            MaterialTheme.colorScheme.onSecondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.onSecondary
-                        }
-                    ),
-                    onClick = { onPenaltySelected(Status.OK) }
-                ) {
-                    Text(
-                        text = "OK",
-                        fontSize = 20.sp
-                    )
-                }
-                Button(
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = if (selectedPenalty == Status.PLUS_TWO) {
-                            MaterialTheme.colorScheme.secondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.secondary
-                        },
-                        contentColor = if (selectedPenalty == Status.PLUS_TWO) {
-                            MaterialTheme.colorScheme.onSecondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.onSecondary
-                        }
-                    ),
-                    onClick = { onPenaltySelected(Status.PLUS_TWO) }
-                ) {
-                    Text(
-                        text = "+2",
-                        fontSize = 20.sp
-                    )
-                }
-                Button(
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = if (selectedPenalty == Status.DNF) {
-                            MaterialTheme.colorScheme.secondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.secondary
-                        },
-                        contentColor = if (selectedPenalty == Status.DNF) {
-                            MaterialTheme.colorScheme.onSecondaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.onSecondary
-                        }
-                    ),
-                    onClick = { onPenaltySelected(Status.DNF) }
-                ) {
-                    Text(
-                        text = "DNF",
-                        fontSize = 20.sp
-                    )
-                }
-                Surface(
-                    shape = MaterialTheme.shapes.medium,
-                    color = MaterialTheme.colorScheme.secondaryContainer,
-                    shadowElevation = 4.dp,
-                ) {
-                    IconButton(onClick = { onDeleteClick() }) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Delete solve",
-                        )
-                    }
-                }
-
-                Surface(
-                    shape = MaterialTheme.shapes.medium,
-                    color = MaterialTheme.colorScheme.secondaryContainer,
-                    shadowElevation = 4.dp,
-                ) {
-                    IconButton(onClick = { onArchiveClick() }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
-                            contentDescription = "Archive solve",
-                        )
-                    }
-                }
-            }
-        }
-
-    }
-}
-
-@Composable
 fun ContentPortrait(
     modifier: Modifier,
     scramble: String,
@@ -208,12 +79,12 @@ fun ContentPortrait(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.weight(1f).widthIn(min = 384.dp).heightIn(min = 128.dp),
             bitmap = image.toImageBitmap(),
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Fit
         )
         Text(
-            modifier = Modifier.weight(1f),
             text = scramble,
             style = MaterialTheme.typography.headlineSmall,
             textAlign = TextAlign.Center
@@ -338,5 +209,140 @@ fun ContentPortrait(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ContentLandscape(
+    modifier: Modifier,
+    image: String,
+    scramble: String,
+    selectedPenalty: Status,
+    onPenaltySelected: (Status) -> Unit,
+    onDeleteClick: () -> Unit,
+    onArchiveClick: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.weight(1f).heightIn(min = 96.dp),
+                bitmap = image.toImageBitmap(),
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                modifier = Modifier.weight(2f),
+                text = scramble,
+                style = MaterialTheme.typography.headlineSmall,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.secondary,
+            shadowElevation = 4.dp,
+            shape = MaterialTheme.shapes.small
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (selectedPenalty == Status.OK) {
+                            MaterialTheme.colorScheme.secondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.secondary
+                        },
+                        contentColor = if (selectedPenalty == Status.OK) {
+                            MaterialTheme.colorScheme.onSecondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.onSecondary
+                        }
+                    ),
+                    onClick = { onPenaltySelected(Status.OK) }
+                ) {
+                    Text(
+                        text = "OK",
+                        fontSize = 20.sp
+                    )
+                }
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (selectedPenalty == Status.PLUS_TWO) {
+                            MaterialTheme.colorScheme.secondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.secondary
+                        },
+                        contentColor = if (selectedPenalty == Status.PLUS_TWO) {
+                            MaterialTheme.colorScheme.onSecondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.onSecondary
+                        }
+                    ),
+                    onClick = { onPenaltySelected(Status.PLUS_TWO) }
+                ) {
+                    Text(
+                        text = "+2",
+                        fontSize = 20.sp
+                    )
+                }
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (selectedPenalty == Status.DNF) {
+                            MaterialTheme.colorScheme.secondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.secondary
+                        },
+                        contentColor = if (selectedPenalty == Status.DNF) {
+                            MaterialTheme.colorScheme.onSecondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.onSecondary
+                        }
+                    ),
+                    onClick = { onPenaltySelected(Status.DNF) }
+                ) {
+                    Text(
+                        text = "DNF",
+                        fontSize = 20.sp
+                    )
+                }
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    shadowElevation = 4.dp,
+                ) {
+                    IconButton(onClick = { onDeleteClick() }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete solve",
+                        )
+                    }
+                }
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    shadowElevation = 4.dp,
+                ) {
+                    IconButton(onClick = { onArchiveClick() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "Archive solve",
+                        )
+                    }
+                }
+            }
+        }
+
     }
 }
