becafb162530b38fe49da982bf64bdf7b065e3cd
sergiomisas <misas_virseda@hotmail.es>
2024-06-13 03:17:02 +0200

    Added app icon for linux, windows and android

--- app/composeApp/src/desktopMain/kotlin/dev/kuromiichi/omnitimer/main.kt
+++ app/composeApp/src/desktopMain/kotlin/dev/kuromiichi/omnitimer/main.kt
@@ -1,5 +1,6 @@
 package dev.kuromiichi.omnitimer
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import dev.kuromiichi.omnitimer.platform.DriverFactory
@@ -7,9 +8,12 @@ import dev.kuromiichi.omnitimer.platform.createDatabase
 import dev.kuromiichi.omnitimer.services.DatabaseService
 
 fun main() = application {
+    val icon = painterResource("drawable/app_icon.png")
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "OmniTimer",
+        icon = icon
     ) {
         // Initialize database
         DatabaseService.apply {
